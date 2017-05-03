7c1dbd1810e6f4db071b9142e11985495a7213f7
Joyce Lin <joycelin79@gmail.com>
2017-05-03 00:20:27 -0700

    update relative links, img, convert html to md

--- parser.js
+++ parser.js
@@ -1,46 +1,68 @@
-var $ = require('cheerio');
+var cheerio = require('cheerio');
 var fs = require('fs');
 var path = require('path');
-
-// let $ = cheerio.load('<h2 class="title">Hello world</h2>');
-//
-// $('h2.title').text('Hello there!');
-// $('h2').addClass('welcome');
-//
-// $.html();
-// //=> <h2 class="title welcome">Hello there!</h2>
+var toMarkdown = require('to-markdown');
 
 // extract the 'main-content' div from HTML file
+// UPDATE relative links
+// UPDATE img links
+// convert HTML to MD
 function scrape(exportedFile) {
 
     var htmlString = fs.readFileSync(exportedFile);
-    var parsedHTML = $.load(htmlString);
+    var $ = cheerio.load(htmlString);
+
+    var pageId = exportedFile.slice(-13,-5);
+    var pageName = exportedFile.slice(4).split("_")[0];
 
     // query for all elements with id 'main-content' and loop over them
-    parsedHTML('#main-content').map(function (i, mainDiv) {
-        // the 'main-content' html element into a cheerio object (same pattern as jQuery)
-        mainDiv = $(mainDiv);
-        console.log(mainDiv.html());
+    var mainDiv = $('#main-content');
+
+    // UPDATE relative links
+    // replace this: https://postmanlabs.atlassian.net/wiki/display/DOC/
+    // with this: https://www.getpostman.com/docs/
+    $('a').each(function(index, aTag) {
+        var $aTag = $(aTag);
+        var newText = $aTag.attr('href').replace("https://postmanlabs.atlassian.net/wiki/display/DOC/", "https://www.getpostman.com/docs/");
+        $aTag.attr('href', newText);
     });
 
+    // UPDATE img links
+    // replace this: "attachments/"
+    // with this: TODO: TBD AWS S3 link
+    // $('img').each(function(index, imgTag) {
+    //     var $imgTag = $(imgTag);
+    //     var newImgText = $imgTag.attr('src').replace("attachments/" + pageId, "TBD");
+    //     $imgTag.attr('src', newImgText);
+    // });
+
+    // convert HTML to MD
+    var markdownPage = toMarkdown(mainDiv.html());
+
+    // write file
+    fs.writeFileSync(pageName + '.md', markdownPage);
+
+    console.log(pageId);
+    console.log(pageName);
+    console.log(mainDiv.html());
 }
 
 var moveFrom = "./DOC";
 var moveTo = "./DOC";
 
-// Loop through files in a folder
+// Loop through entries in a folder
 // http://stackoverflow.com/questions/32511789/looping-through-files-in-a-folder-node-js
-fs.readdir( moveFrom, function( err, files ) {
+fs.readdir(moveFrom, function(err, entries) {
 
     if(err) {
         console.error("Could not list the directory.", err);
         return;
     }
 
-    files.forEach( function( file, index ) {
+    entries.forEach(function(entry, index) {
         // Make one pass and make the file complete
-        var fromPath = path.join( moveFrom, file );
-        var toPath = path.join( moveTo, file );
+        var fromPath = path.join(moveFrom, entry);
+        var toPath = path.join(moveTo, entry);
 
         fs.stat( fromPath, function( error, stat ) {
             if(error) {
@@ -48,8 +70,9 @@ fs.readdir( moveFrom, function( err, files ) {
                 return;
             }
 
-            if(stat.isFile()) {
+            if(stat.isFile() && entry != ".DS_Store") {
                 console.log("'%s' is a file.", fromPath);
+                console.log("Scraping '%s'", fromPath);
                 scrape(fromPath);
             } else if(stat.isDirectory()) {
                 console.log("'%s' is a directory.", fromPath);
@@ -62,26 +85,9 @@ fs.readdir( moveFrom, function( err, files ) {
             //     else {
             //         console.log("Moved file '%s' to '%s'.", fromPath, toPath);
             //     }
-            // } );
-        } );
-    } );
-} );
+            // });
+        });
+    });
+});
 
 // scrape('./DOC/Authorization_58930556.html');
-
-// let $ = cheerio.load(fs.readFileSync('./DOC/Authorization_58930556.html'));
-//
-// var content = $('#main-content > div')
-//
-// function pullData(err, data) {
-//     var $ = cheerio.load('
-//
-// $('#main-content > div').each(function(i, elem) {
-//     var id = $(elem).attr('id'),
-//         filename = id + '.html',
-//         content = $.html(elem);
-//     fs.writeFile(filename, content, function(err) {
-//         console.log('Written html to ' + filename);
-//     });
-// });
-// }
