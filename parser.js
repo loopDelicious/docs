a0785e8990773201bd71830060f35b76afb16f6a
Joyce Lin <joycelin79@gmail.com>
2017-05-03 17:50:02 -0700

    working on data types for HTML sanitization

--- parser.js
+++ parser.js
@@ -10,15 +10,20 @@ var toMarkdown = require('to-markdown');
 // convert HTML to MD
 function scrape(exportedFile) {
 
-    var htmlString = fs.readFileSync(exportedFile);
-    var sanitized = sanitizeHtml(htmlString);
-    var $ = cheerio.load(sanitized);
-
     var pageId = exportedFile.slice(-13,-5);
     var pageName = exportedFile.slice(4).split("_")[0];
 
-    // query for all elements with id 'main-content' and loop over them
-    var mainDiv = $('#main-content');
+    // read in, and sanitize HTML by removing unwanted CSS
+    var htmlString = fs.readFileSync(exportedFile);
+    // console.log("htmlString " + typeof(htmlString)); // object
+    // console.log("htmlString " + htmlString); // object
+    var sanitized = sanitizeHtml(htmlString);
+    // console.log("sanitized " + typeof(sanitized)); // string
+    // console.log("sanitized " + sanitized); // string
+    var sanitizedObj = JSON.parse(sanitized);
+    // parse HTML
+    // var $ = cheerio.load(sanitized);
+    var $ = cheerio.load(sanitizedObj);
 
     // UPDATE relative links
     // replace this: https://postmanlabs.atlassian.net/wiki/display/DOC/
@@ -30,7 +35,7 @@ function scrape(exportedFile) {
     });
 
     // UPDATE img links
-    // replace this: src="attachments/58388634/59135435.png...
+    // replace src="attachments/58388634/59135435.png"
     // with this: "https://s3.amazonaws.com/postman-static-getpostman-com/postman-docs/59135435.png"
     $('img').each(function(index, imgTag) {
         var $imgTag = $(imgTag);
@@ -40,60 +45,61 @@ function scrape(exportedFile) {
         $imgTag.attr('src', newLink);
     });
 
+    // pull out element with #main-content
+    var mainDiv = $('#main-content');
+
     // convert HTML to MD
-    var markdownPage = toMarkdown(mainDiv.html(), {
-        converters:
-        [ {
-            filter: function (node) {
-                return node.nodeName === 'span';
-            },
-            replacement: function (content) {
-                return "";
-            }
-        }]
-    });
+    // console.log(typeof(mainDiv.html()));
+    // console.log('main div ' + mainDiv.html());
+    var markdownPage = toMarkdown(mainDiv.html());
+
+    console.log('after md conversion');
 
     // write file
     fs.writeFileSync(pageName + '.md', markdownPage);
 
     console.log(pageId);
     console.log(pageName);
-    console.log(mainDiv.html());
+    // console.log(mainDiv.html());
 }
 
-var moveFrom = "./DOC"; // HTML pages exported from wiki to crawl
-var moveTo = "./DOC";
-
-// Loop through entries in a folder
+// Crawl through entries in a folder containing HTML pages exported from wiki
 // http://stackoverflow.com/questions/32511789/looping-through-files-in-a-folder-node-js
-fs.readdir(moveFrom, function(err, entries) {
+function crawler(moveFrom, moveTo) {
 
-    if(err) {
-        console.error("Could not list the directory.", err);
-        return;
-    }
+    fs.readdir(moveFrom, function (err, entries) {
 
-    entries.forEach(function(entry, index) {
-        // Make one pass and make the file complete
-        var fromPath = path.join(moveFrom, entry);
-        var toPath = path.join(moveTo, entry);
+        if (err) {
+            console.error("Could not list the directory.", err);
+            return;
+        }
 
-        fs.stat( fromPath, function( error, stat ) {
-            if(error) {
-                console.error("Error stating file.", error);
-                return;
-            }
+        entries.forEach(function (entry, index) {
 
-            if(stat.isFile() && entry != ".DS_Store") {
-                console.log("'%s' is a file.", fromPath);
-                console.log("Scraping '%s'", fromPath);
-                scrape(fromPath);
-            } else if(stat.isDirectory()) {
-                console.log("'%s' is a directory.", fromPath);
-            }
+            // Make one pass and make the file complete
+            var fromPath = path.join(moveFrom, entry);
+            var toPath = path.join(moveTo, entry);
 
+            fs.stat(fromPath, function (error, stat) {
+
+                if (error) {
+                    console.error("Error stating file.", error);
+                    return;
+                }
+
+                if (stat.isFile() && entry != ".DS_Store") {
+                    console.log("'%s' is a file.", fromPath);
+                    console.log("Scraping '%s'", fromPath);
+                    scrape(fromPath);
+                } else if (stat.isDirectory()) {
+                    console.log("'%s' is a directory.", fromPath);
+                }
+
+            });
         });
     });
-});
+}
+
+crawler("./DOC", "./DOC");
 
 // scrape('./DOC/Authorization_58930556.html');
